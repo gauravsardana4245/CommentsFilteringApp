0e2b11f8833d251a7f3cea8d9456cb3e55c5e03f
gauravsardana4245 <gaurravsarrdana@gmail.com>
2023-10-09 21:17:28 +0530

    Removed Global Styles

--- styles/global.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const globalStyles = StyleSheet.create({
-    commentItem: {
-        marginTop: 10,
-        borderColor: 'black',
-        borderWidth: 2
-    }
-})
